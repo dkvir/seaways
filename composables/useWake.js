b2066943962c336b7abfeecb1f9545dade7617e7
dkvir <kvirika137@gmail.com>
2025-05-28 13:46:39 +0400

    👌 IMPROVE: removed debug mode

--- composables/useWake.js
+++ composables/useWake.js
@@ -52,7 +52,6 @@ export const useWake = class WakeEffect {
     // Higher resolution for smoother surface matching
     this.wakeResolution = { width: 64, length: 128 };
     this.time = 0;
-    this.debug = false;
     this.clock = new THREE.Clock();
   }
 
@@ -93,21 +92,6 @@ export const useWake = class WakeEffect {
 
     // Add to scene
     this.scene.add(this.wakePlane);
-
-    // Add debug helpers if in debug mode
-    if (this.debug) {
-      const axisHelper = new THREE.AxesHelper(10);
-      this.wakePlane.add(axisHelper);
-
-      const wireframe = new THREE.WireframeGeometry(this.wakeGeometry);
-      const line = new THREE.LineSegments(wireframe);
-      line.material.color.set(0xff0000);
-      line.material.opacity = 0.25;
-      line.material.transparent = true;
-      this.wakePlane.add(line);
-
-      console.log("Wake plane initialized with debug mode:", this.wakePlane);
-    }
   }
 
   createEnhancedFoamShader() {
